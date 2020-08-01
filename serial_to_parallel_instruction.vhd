10e3e24e4b35d4bbfdfedfe6c9887e43243bfa01
Henry <henry.rabina@gmail.com>
2020-08-01 16:43:43 +0300

    Improve state_machine logic

--- serial_to_parallel_instruction.vhd
+++ serial_to_parallel_instruction.vhd
@@ -16,11 +16,10 @@ end serial_to_parallel_instruction;
 
 architecture rtl of serial_to_parallel_instruction is
 	signal code_word : std_logic_vector(5 downto 0);
+	signal counter : integer := 0;
 begin
 
 	func: process(all) is
-		variable counter : integer := 0;
-		variable state_machine_counter : integer := 0;
 	begin
 
 		if (reset = '1') then
@@ -28,35 +27,34 @@ begin
 			opcode_out <= (others => '0');
 			code_word <= (others => '0');
 			trig_state_machine <= '0';
-			counter := 0;
-			state_machine_counter := 0;
+			counter <= 0;
 
 		elsif (rising_edge(clk)) then
 
 			if (counter >= N + M) then
-				if (state_machine_counter >= 120) then
-					trig_state_machine <= '0';
-					counter := 0;
+				trig_state_machine <= '1';
+				if (counter < N + M + 5) then
+					counter <= counter + 1;
+				else
 					code_word <= (others => '0');
+					counter <= 0;
 				end if;
-				state_machine_counter := state_machine_counter + 1;
 
 			elsif (counter >= M and counter <= N + M - 1 and code_word = "000110") then
 				operand_out <= operand_out(N-2 downto 0) & serial_in;
-				trig_state_machine <= '1';
-				counter := counter + 1;
+				trig_state_machine <= '0';
+				counter <= counter + 1;
 
 			elsif (counter <= M - 1 and code_word = "000110") then
 				opcode_out <= opcode_out(M-2 downto 0) & serial_in;
 				operand_out <= (others => '0');
 				trig_state_machine <= '0';
-				counter := counter + 1;
+				counter <= counter + 1;
 
 			elsif (counter = 0) then
 				opcode_out <= (others => '0');
 				operand_out <= (others => '0');
 				trig_state_machine <= '0';
-				state_machine_counter := 0;
 				code_word <= code_word(4 downto 0) & serial_in;
 
 			end if;
--- state_machine.vhd
+++ state_machine.vhd
@@ -16,11 +16,11 @@ architecture rtl of state_machine is
 
 	type t_state is (do_alu, do_wreg, do_result);
 	signal state : t_state;
+	signal delay : integer := 10;
 	
 begin
 
 	func : process(all) is
-		variable counter : integer := 0;
 	begin
 
 		if (reset = '1') then
@@ -28,37 +28,41 @@ begin
 			wreg_enable <= '0';
 			result_enable <= '0';
 			state <= do_alu;
-			counter := 0;
+			delay <= 10;
 			
 		elsif (rising_edge(clk)) then
-			case state is
 
+			if delay > 0 then
+				delay <= delay - 1;
+			end if;
+
+			case state is
 				when do_alu =>
-					alu_enable <= '1';
 					if (trig_state_machine = '1') then
-						counter := counter + 1;
+						if delay = 0 then
+							delay <= 10;
+							alu_enable <= '1';
+							wreg_enable <= '0';
+							result_enable <= '0';
+							state <= do_wreg;
+						end if;
 					end if;
-					if (counter = 100) then
-						counter := 0;
-						alu_enable <= '0';
-						state <= do_wreg;
-					end if;
-					
+
 				when do_wreg =>
-					wreg_enable <= '1';
-					counter := counter + 1;
-					if (counter = 100) then
-						counter := 0;
-						wreg_enable <= '0';
+					if delay = 0 then
+						delay <= 10;
+						alu_enable <= '0';
+						wreg_enable <= '1';
+						result_enable <= '0';
 						state <= do_result;
 					end if;
 				
 				when do_result =>
-					result_enable <= '1';
-					counter := counter + 1;
-					if (counter = 100) then
-						counter := 0;
-						result_enable <= '0';
+					if delay = 0 then
+						delay <= 10;
+						alu_enable <= '0';
+						wreg_enable <= '0';
+						result_enable <= '1';
 						state <= do_alu;
 					end if;
 
